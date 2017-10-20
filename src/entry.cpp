236fccc68da23389512a8476d3a72a5ef466bba4
Zbigniew Romanowski <romz@wp.pl>
2017-10-20 13:51:31 +0200

    Not important change

--- src/entry.cpp
+++ src/entry.cpp
@@ -1,6 +1,5 @@
 #include "entry.h"
 #include <stdexcept>
-#include <utility>
 
 //
 //
@@ -17,13 +16,11 @@ Entry::Entry( const Key& key )
 //
 Data Entry::Get( const Key& key ) const
 {
-    using namespace std::rel_ops;
-
     if( key != m_key || !m_bucket )
     {
         throw std::runtime_error( "Key is not found" );
     }
 
-    return m_bucket->Get();
+    return m_bucket->Get( key );
 
 }
