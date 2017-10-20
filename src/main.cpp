c4516f3ccdc97d698f224eaf90ab3b7661cb2c30
Zbigniew Romanowski <romz@wp.pl>
2017-10-20 10:39:56 +0200

    Test for HashEx::Get function

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include "hashex.h"
+
+void TestGet();
 
 int main()
 {
-    std::cout << "OK\n";
+    try
+    {
+        TestGet();
+    }
+    catch( std::exception& e )
+    {
+        std::cout << e.what() << std::endl << std::flush;
+        return 1;
+    }
+
+    std::cout << "OK\n" << std::flush;
     return 0;
+}
+
+//
+//
+//
+void TestGet()
+{
+    HashEx he;
+    Key key( 1 );
+    Data data = he.Get( key );
 }
 
