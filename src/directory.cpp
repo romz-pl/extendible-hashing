b1535dfafebb2f40cdbc4e56080169a2eaf6f1b2
Zbigniew Romanowski <romz@wp.pl>
2017-10-20 10:39:29 +0200

    Check if directory is empty

--- src/directory.cpp
+++ src/directory.cpp
@@ -1,5 +1,6 @@
 #include "directory.h"
 #include <cassert>
+#include <stdexcept>
 
 //
 //
@@ -15,6 +16,11 @@ Directory::Directory()
 //
 Data Directory::Get( const Key& key ) const
 {
+    if( m_dir.size() == 0 )
+    {
+        throw std::runtime_error( "The directory is empty." );
+    }
+
     std::size_t id = GetEntryId( key );
     return m_dir[ id ].Get( key );
 }
