e3547bf283e6b8eea2a54807be54c08aa2673bda
Zbigniew Romanowski <romz@wp.pl>
2017-10-23 10:51:40 +0200

    Named constants for clarity

--- src/key.cpp
+++ src/key.cpp
@@ -24,8 +24,10 @@ std::size_t Key::GetHash() const
 //
 uint32_t Key::Hash32( uint32_t x )
 {
-    x = ((x >> 16) ^ x) * 0x45d9f3b;
-    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    const uint32_t a = 0x45d9f3b;
+
+    x = ((x >> 16) ^ x) * a;
+    x = ((x >> 16) ^ x) * a;
     x = (x >> 16) ^ x;
     return x;
 }
@@ -35,8 +37,11 @@ uint32_t Key::Hash32( uint32_t x )
 //
 uint64_t Key::Hash64( uint64_t x )
 {
-    x = (x ^ (x >> 30)) * UINT64_C(0xbf58476d1ce4e5b9);
-    x = (x ^ (x >> 27)) * UINT64_C(0x94d049bb133111eb);
+    const uint64_t a = 0xbf58476d1ce4e5b9;
+    const uint64_t b = 0x94d049bb133111eb;
+
+    x = (x ^ (x >> 30)) * a;
+    x = (x ^ (x >> 27)) * b;
     x = x ^ (x >> 31);
     return x;
 }
