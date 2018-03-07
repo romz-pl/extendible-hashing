1461f9d555ef2fc00ba50dd30ed8c7f63acfa5af
Zbigniew Romanowski <romz@wp.pl>
2018-03-07 19:38:34 +0100

    Add test for Key class

---
+++ tests/key.cpp
@@ -0,0 +1,94 @@
+#include <gtest/gtest.h>
+#include "hashex.h"
+#include <random>
+#include <algorithm>
+#include <limits>
+
+//
+//
+//
+TEST(key, Compare)
+{
+
+    std::random_device rd;
+    std::mt19937 g( rd() );
+
+    std::uniform_int_distribution< uint32_t > pick( 0, std::numeric_limits<uint32_t>::max() );
+
+    for( size_t i = 0; i < 10000; i++ )
+    {
+        const uint32_t v1 = pick( g );
+        const uint32_t v2 = pick( g );
+        
+        EXPECT_TRUE( Key( v1 ) == Key( v1 ) );
+        EXPECT_TRUE( Key( v2 ) == Key( v2 ) );
+        
+        if( v1 == v2 )
+        {
+            EXPECT_TRUE( Key( v1 ) == Key( v2 ) );
+        }
+        
+        if( v1 != v2 )
+        {
+            EXPECT_TRUE( Key( v1 ) != Key( v2 ) );
+        }
+        
+        if( v1 < v2 )
+        {
+            EXPECT_TRUE( Key( v1 ) < Key( v2 ) );
+        }
+        
+        if( v1 <= v2 )
+        {
+            EXPECT_TRUE( Key( v1 ) <= Key( v2 ) );
+        }
+        
+        if( v1 > v2 )
+        {
+            EXPECT_TRUE( Key( v1 ) > Key( v2 ) );
+        }
+        
+        if( v1 >= v2 )
+        {
+            EXPECT_TRUE( Key( v1 ) >= Key( v2 ) );
+        }
+        
+    }
+}
+
+//
+//
+//
+TEST(key, ToString)
+{
+    std::random_device rd;
+    std::mt19937 g( rd() );
+
+    std::uniform_int_distribution< uint32_t > pick( 0, std::numeric_limits<uint32_t>::max() );
+
+    for( size_t i = 0; i < 10000; i++ )
+    {
+        const uint32_t v = pick( g );
+        const Key key( v );
+        EXPECT_TRUE( !key.ToString().empty() );
+    }
+}
+
+//
+//
+//
+TEST(key, GetHash)
+{
+    std::random_device rd;
+    std::mt19937 g( rd() );
+
+    std::uniform_int_distribution< uint32_t > pick( 0, std::numeric_limits<uint32_t>::max() );
+
+    for( size_t i = 0; i < 10000; i++ )
+    {
+        const uint32_t v = pick( g );
+        const Key key( v );
+        EXPECT_NO_THROW( key.GetHash() );
+    }
+}
+
